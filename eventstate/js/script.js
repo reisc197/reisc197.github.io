0d2159b6a02d608e7f834ff564d58ba82c30a5bb
Costanza <reisc197@newschool.edu>
2017-02-23 11:39:40 -0500

    write up commit

---
+++ eventstate/js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	console.log("hello there!");
+
+	$(".button").on("mouseenter", function() {
+		//here are the hover instruction
+		$(this). addClass("focused");
+	}); 
+
+	$(".button").on("mouseleave", function(){
+		$(this).removeClass("focused");
+	});
+
+	$(".button").on("click", function(){
+		$(this).toggleClass("active");
+		$("body").toggleClass("dark");
+		$("h1").html("Clicked!");
+	});
+
+	$("h1").on("click", function(){
+		$(this).html("Switch");
+
+	});
+
+
+
+
+}); 
