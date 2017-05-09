a36d0afbc5777a2fc63998d8f360c7d8d62a1e40
Costanza <reisc197@newschool.edu>
2017-05-09 17:37:55 -0400

    trashh commit

---
+++ trash/trash.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){ 
+    "use strict"; 
+
+     $(".compost").mouseenter(function(){
+     $(".unrecyclable1").css("opacity", "0");
+     $(".recyclable1").css("opacity", "0");
+     $(".all1").css("opacity", "0");
+});
+
+     $(".compost").mouseleave(function(){
+     $(".unrecyclable1").css("opacity", "1");
+     $(".recyclable1").css("opacity", "1");
+     $(".all1").css("opacity", "1");
+});
+
+     $(".unrecyclable").mouseenter(function(){
+     $(".compost1").css("opacity", "0");
+     $(".recyclable1").css("opacity", "0");
+     $(".all1").css("opacity", "0");
+});
+
+     $(".unrecyclable").mouseleave(function(){
+     $(".compost1").css("opacity", "1");
+     $(".recyclable1").css("opacity", "1");
+     $(".all1").css("opacity", "1");
+});
+
+     $(".recyclable").mouseenter(function(){
+     $(".compost1").css("opacity", "0");
+     $(".unrecyclable1").css("opacity", "0");
+     $(".all1").css("opacity", "0");
+});
+
+     $(".recyclable").mouseleave(function(){
+     $(".compost1").css("opacity", "1");
+     $(".unrecyclable1").css("opacity", "1");
+     $(".all1").css("opacity", "1");
+});
+
+
+
+
+});
+
+
